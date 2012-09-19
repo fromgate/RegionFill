365c0fbfeb5618c0b879b7c3d5e2ed51903c9b66
galaran <galaran.h@gmail.com>
2012-09-19 10:14:34 +0400

    + Force build for prefixed regions

--- src/fromgate/regionfill/RegionFill.java
+++ src/fromgate/regionfill/RegionFill.java
@@ -1,63 +1,115 @@
 package fromgate.regionfill;
 
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
-import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+public class RegionFill extends JavaPlugin implements Listener {
 
-public class RegionFill extends JavaPlugin {
-	
-	WorldGuardPlugin worldguard;
-	boolean wg_active=false;
-	
-	@Override
-	public void onEnable() {
-		wg_active= connectWorldGuard();
-	}
+    private WorldGuardPlugin worldGuard;
+    private boolean wgActive = false;
 
-	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-		if ((sender instanceof Player)&&(!((Player) sender).hasPermission("regionfill.execute"))) return true;
-		if(cmd.getName().equalsIgnoreCase("rgfill")&&(args.length==3)){
-			if (!args[2].matches("[0-9]*")) return true;
-			int id = Integer.parseInt(args[2]);
-			
-			World w = Bukkit.getWorld(args[0]);
-			if (w == null) return true;
-			ProtectedRegion rg = worldguard.getRegionManager(w).getRegion(args[1]);
-			if (rg == null) return true;
-			int minx = rg.getMinimumPoint().getBlockX();
-			int miny = rg.getMinimumPoint().getBlockY();
-			int minz = rg.getMinimumPoint().getBlockZ();
-			int maxx = rg.getMaximumPoint().getBlockX();
-			int maxy = rg.getMaximumPoint().getBlockY();
-			int maxz = rg.getMaximumPoint().getBlockZ();
-			
-			for (int x = minx; x<=maxx;x++)
-				for (int z = minz; z<=maxz;z++)
-					for (int y = miny; y<=maxy;y++){
-						Block b = w.getBlockAt(x, y, z);
-						b.setTypeId(id);
-					}
-		}
-		return false;
-	}
-	
-	
-	
-	public boolean connectWorldGuard(){
-		Plugin worldGuard = getServer().getPluginManager().getPlugin("WorldGuard");
-		if ((worldGuard != null)&&(worldGuard instanceof WorldGuardPlugin)) {
-			worldguard = (WorldGuardPlugin)worldGuard;
-			return true;
-		}
-		return false;
-	}
+    private static final String RG_BYPASS_PREFIX = "_fb_"; // force build
 
+    @Override
+    public void onEnable() {
+        connectWorldGuard();
+        if (wgActive) {
+            getServer().getPluginManager().registerEvents(this, this);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onBlockBreak(BlockBreakEvent event) {
+        checkForceBuild(event.getBlock(), event);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onBlockPlace(BlockPlaceEvent event) {
+        checkForceBuild(event.getBlock(), event);
+    }
+
+    private void checkForceBuild(Block block, Cancellable event) {
+        if (!event.isCancelled()) return;
+
+        ApplicableRegionSet regions = worldGuard.getRegionManager(block.getWorld()).getApplicableRegions(block.getLocation());
+        for (ProtectedRegion curRegion : regions) {
+            if (curRegion.getId().startsWith(RG_BYPASS_PREFIX)) {
+                event.setCancelled(false);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("rgfill") && args.length == 3) {
+            if (!wgActive) {
+                sender.sendMessage(ChatColor.RED + "World Guard not active");
+                return true;
+            }
+
+            int id;
+            try {
+                id = Integer.parseInt(args[2]);
+                if (id < 0) throw new NumberFormatException();
+            } catch (NumberFormatException ex) {
+                sender.sendMessage(ChatColor.RED + "Id must be a positive number");
+                return true;
+            }
+
+            World world = Bukkit.getWorld(args[0]);
+            if (world == null) {
+                sender.sendMessage(ChatColor.RED + "World " + args[0] + " not loaded");
+                return true;
+            }
+
+            ProtectedRegion rg = worldGuard.getRegionManager(world).getRegion(args[1]);
+            if (rg == null) {
+                sender.sendMessage(ChatColor.RED + "Region " + args[1] + " not exists in the world " + world.getName());
+                return true;
+            }
+
+            int minX = rg.getMinimumPoint().getBlockX();
+            int minY = rg.getMinimumPoint().getBlockY();
+            int minZ = rg.getMinimumPoint().getBlockZ();
+            int maxX = rg.getMaximumPoint().getBlockX();
+            int maxY = rg.getMaximumPoint().getBlockY();
+            int maxZ = rg.getMaximumPoint().getBlockZ();
+
+            for (int x = minX; x <= maxX; x++) {
+                for (int z = minZ; z <= maxZ; z++) {
+                    for (int y = minY; y <= maxY; y++) {
+                        world.getBlockAt(x, y, z).setTypeId(id);
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public void connectWorldGuard() {
+        Plugin worldGuardPlugin = getServer().getPluginManager().getPlugin("WorldGuard");
+        if ((worldGuardPlugin != null) && (worldGuardPlugin instanceof WorldGuardPlugin)) {
+            worldGuard = (WorldGuardPlugin) worldGuardPlugin;
+            wgActive = true;
+        }
+    }
 }
